0ee5f416fff8660aa1c542864f27bc10fe86d08f
Waqas <m_waqas_hameed@hotmail.com>
2020-03-28 01:04:19 +0500

    Quotes app without delete functionality

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(text: 'To be the best, beat the best', author: 'Someone Great'),
+    Quote(
+        text: 'If you can\'t make it good atleast make it look good',
+        author: 'Bill Gates'),
+    Quote(
+        text: 'Hardwork beats talent if talent doesn\'t work hard',
+        author: 'Someone Great'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+      ),
+    );
+  }
+}
